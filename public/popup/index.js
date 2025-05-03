198bcc598c74696ab0817d60912169f078024e70
gitguru <robert43us@gmail.com>
2025-05-03 17:16:31 +0600

    popdeleted

--- public/popup/index.js
+++
@@ -1,2 +0,0 @@
-// This file ensures the popup directory is properly recognized by the server
-console.log("Popup directory loaded"); 
--- src/components/VerificationPopup.jsx
+++ src/components/VerificationPopup.jsx
@@ -1,5 +1,4 @@
 import { useEffect, useState } from 'react';
-import '/popup/popstyle.css';
 
 const VerificationPopup = () => {
   /*
